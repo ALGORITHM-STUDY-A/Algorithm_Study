aea579dd29117b70635919d8d696a1340892f36e
전재연 <sunrox1107@gmail.com>
2024-10-28 20:48:35 +0900

    [test] PR test

--- JaeYeon/test/test.py
+++ JaeYeon/test/test.py
@@ -1,13 +1,1 @@
-def solution(numer1, denom1, numer2, denom2):
-    answer = []
-
-    answer.append(numer1 + numer2)
-    answer.append(denom1, denom2)
-
-    if answer[0] % answer[0] == 0:
-        answer[0] == answer[0] // answer[1]
-        answer[1] == answer[1] // answer[0]
-
-    return answer
-
-solution(1,2,3,4)
+# test
